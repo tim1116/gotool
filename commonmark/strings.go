f0e1aeea5a354ab2f3208560c08fc11550693385
tim <657978106@qq.com>
2020-11-22 21:03:02 +0800

    add commonmark.Trim() function

--- commonmark/strings.go
+++ commonmark/strings.go
@@ -3,6 +3,7 @@ package commonmark
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"strings"
 )
 
 // md5.
@@ -10,4 +11,9 @@ func Md5(str string) string {
 	h := md5.New()
 	h.Write([]byte(str))
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+// 只用于去除字符串前后空格使用
+func Trim(str string) string {
+	return strings.Trim(str, " ")
 }
--- commonmark/strings_test.go
+++ commonmark/strings_test.go
@@ -12,3 +12,14 @@ func TestMd5(t *testing.T) {
 		t.Errorf("Md5() failed. Got %s, expected 098f6bcd4621d373cade4e832627b4f6.", r)
 	}
 }
+
+func TestTrim(t *testing.T) {
+	ss := " abc "
+	if len(ss) != 5 {
+		t.FailNow()
+	}
+	nss := Trim(ss)
+	if len(nss) != 3 {
+		t.FailNow()
+	}
+}
